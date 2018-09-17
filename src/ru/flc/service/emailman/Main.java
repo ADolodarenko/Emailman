618cd0cbb83ece2a8a04fc7e39bb2e3daf214b61
ADolodarenko <adolodar@gmail.com>
2018-09-17 17:18:51 +0300

    First commit.

---
+++ src/ru/flc/service/emailman/Main.java
@@ -0,0 +1,9 @@
+package ru.flc.service.emailman;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I'll build a visual app for sending e-mails from a database here.");
+    }
+}
